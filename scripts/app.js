f1d3fcc82e22595d5204725d9a9514f25f86c1c6
Theodor Borgman <theo@oxxygen.io>
2019-09-05 20:49:58 +0200

    changed the timer to display 10th of seconds. fond error that needs to be fixed later

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,17 @@ var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = end - now;
     // Output the result in an element "Timer"
-    document.getElementById("timer").innerHTML = distance / 1000;
+    document.getElementById("timer").innerHTML = distance / 100;
 
     // If the count down is over
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("timer").innerHTML = "SLUT!";
+        document.getElementById("word1").innerHTML = "Base!";
+        document.getElementById("word2").innerHTML = "Base!";
+        document.getElementById("word3").innerHTML = "BASS!";
+        document.getElementById("word4").innerHTML = "BESS!";
+        document.getElementById("word5").innerHTML = "BOZE!";
+        document.getElementById("word6").innerHTML = "ESAB!";
     }
-}, 1000);
+}, 100);
